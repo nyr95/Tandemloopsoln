776889ba4384f9311f5852cc0c7ed4996168e524
Nagraj Y R <103982231+nyr95@users.noreply.github.com>
2025-05-21 22:50:16 +0530

    Delete Program-2.py

--- Program-2.py
+++
@@ -1,8 +0,0 @@
-def generate_series_count_based(x: int):
-    result = []
-    for i in range(x):
-        result.append(2 * i + 1)
-    print(", ".join(map(str, result)))
-
-
-generate_series_count_based(4)  
